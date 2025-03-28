7093500ca58799518c297a692626041d29009580
Administrator <306340032@qq.com>
2025-03-29 04:18:23 +0800

    完善LLM模式模块2

--- app/agent/cogniself.py
+++ app/agent/cogniself.py
@@ -1,9 +1,11 @@
 #CogniSelf模块
 from app.agent.browser import BrowserAgent
 
+
 class CogniSelf(BrowserAgent):#CognSelf类继承自BrowserAgent类
     name: str = "CogniSelf"
     version: str = "0.1.0"
     description: str = "一个多功能代理，可以使用多种工具解决各种任务."
-
     print("CogniSelf类正在运行...，父类：BrowserAgent")
+
+
